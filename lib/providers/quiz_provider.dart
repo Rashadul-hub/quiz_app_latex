9f24e6fcaeb7a555f32c0addd761bb8d9d97b3b6
Rashadul-hub <rashadulislamhimu@gmail.com>
2025-08-16 14:48:54 +0600

    Timer Functionality Implemeneted

---
+++ lib/providers/quiz_provider.dart
@@ -0,0 +1,91 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+
+import '../models/question.dart';
+
+class QuizProvider extends ChangeNotifier{
+  final List<Question> _all;
+  late List<Question> _questions;
+  int _index = 0;
+  int _score = 0;
+  int? _selected;
+  String _category = 'All';
+
+  final int perQuestionSeconds;
+  int _secondsLeft;
+  Timer? _timer;
+
+  QuizProvider(this._all,{this.perQuestionSeconds =15})
+      : _secondsLeft = perQuestionSeconds{
+    _questions = _all;
+  }
+
+
+  List<String> get categories => [
+    'All',
+    ...{
+      for (final q in _all) q.category
+    }];
+
+  String get category => _category;
+  int get index => _index;
+  int get total => _questions.length;
+  int get score => _score;
+  int get? selected => _selected;
+  int get secondsLeft => _secondsLeft;
+  Question get current => _questions[_index];
+
+
+  void setCategory(String c){
+    _category = c;
+    _questions = c == 'All' ? _all.where((q) => q.category == c).toList();
+    _resetQuiz();
+    notifyListeners();
+  }
+
+  void startTimer(void Function() onTick, void Function() onTimeout){
+    _timer?.cancel();
+    _secondsLeft = perQuestionSeconds;
+    _timer = Timer.periodic(const Duration(seconds: 1), (_){
+      if(--_secondsLeft <= 0){
+        _timer?.cancel();
+        _selected ??= -1;
+        onTimeout();
+      }
+      onTick();
+    });
+  }
+
+  void stopTimer(){
+    _timer?.cancel();
+  }
+
+  void select(int i){
+    if(_selected!=null) return;
+    _selected=i;
+    if(i==current.answerIndex) _score++;
+    notifyListeners();
+  }
+
+
+  bool get isLast => _index==total-1;
+
+
+  void next(){
+    if(_selected==null) return;
+    _index++;
+    _selected=null;
+    _secondsLeft=perQuestionSeconds;
+    notifyListeners();
+  }
+
+  void _resetQuiz(){
+    if(_selected == null) return;
+    _index ++;
+    _selected = null;
+
+  }
+
+
+}
