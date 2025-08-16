437ef188672deee12e5592261eca52c00120ad52
Rashadul-hub <rashadulislamhimu@gmail.com>
2025-08-16 15:51:39 +0600

    Complete Custom Reusable Componenet OptionTile

--- lib/widgets/option_tile.dart
+++ lib/widgets/option_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_latex/widgets/latex_text.dart';
 
 class OptionTile extends StatelessWidget {
   final String label;
@@ -11,13 +12,34 @@ class OptionTile extends StatelessWidget {
 
   Color _color(BuildContext context){
     if(locked) return Theme.of(context).colorScheme.surfaceContainerHighest;
-    if(correct) return Colors.green;
-    if(selected) return Colors.red;
-    return Colors.grey;
+    if(selected && correct) return Colors.green.withOpacity(.2);
+    if(selected && !correct) return Colors.red.withOpacity(.2);
+    if(correct) return Colors.green.withOpacity(.15);
+    return Theme.of(context).colorScheme.surfaceContainerHighest;
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    return InkWell(
+      onTap: locked? null : onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        padding: const EdgeInsets.all(12),
+        margin:  const EdgeInsets.symmetric(vertical: 6),
+        decoration: BoxDecoration(
+          color: _color(context),
+          borderRadius: BorderRadius.circular(14),
+          border: Border.all(
+            color: selected ? Theme.of(context).colorScheme.primary : Colors.transparent,
+            width: 1,
+          ),
+        ),
+        child: LatexText(
+          tex: label,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+      ),
+    );
   }
 }
